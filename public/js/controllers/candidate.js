79ca581a6f7a86b324015b7bf775af62546db31b
Aladdin <alaaden3000@gmail.com>
2017-11-08 17:37:36 +0200

    add login logo

--- public/js/controllers/candidate.js
+++ public/js/controllers/candidate.js
@@ -498,13 +498,6 @@
       );
       $scope.editCandidate = function() {
         $scope.laddaStatus = true;
-        $scope.editCandidateForm.attachment = $.map(
-          $scope.editCandidateForm.attachment,
-          function(value, index) {
-            console.log([index]);
-            return [index];
-          }
-        );
         candidateService
           .editCandidate($stateParams.id, $scope.editCandidateForm)
           .then(
